c0cd680df654cf2c299c71c5df295c88248d97a3
Konrad <jupterkdh@gmail.com>
2023-05-03 17:02:36 -0300

    create avatar

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,14 +1,20 @@
-const { Router } = require("express");
+const { Router, request } = require("express");
+const multer = require("multer");
+const uploadConfig = require("../configs/upload")
 
 const UsersController = require("../controllers/usersController");
-const ensureAuthenticated =  require("../middlewares/ensureAuthenticated")
+const UserAvatarController = require("../controllers/UserAvatarController");
+const ensureAuthenticated =  require("../middlewares/ensureAuthenticated");
 
 const usersRouter = Router();
+const upload = multer(uploadConfig.MULTER);
 
 const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
 
 //usersRouter.use(myMiddleware); // para user em todos
 usersRouter.post("/", usersController.create);// se for usar o middleware em alguma rota especifica 
 usersRouter.put("/", ensureAuthenticated, usersController.update);
+usersRouter.patch("/avatar", ensureAuthenticated, upload.single("avatar"), userAvatarController.update);
 
 module.exports = usersRouter;
