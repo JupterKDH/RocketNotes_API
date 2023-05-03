a8316b0027cb53e5c3a8628e53af737c898647f9
Konrad <jupterkdh@gmail.com>
2023-05-03 16:07:52 -0300

    created upload function

--- src/routes/tags.routes.js
+++ src/routes/tags.routes.js
@@ -1,12 +1,13 @@
 const { Router } = require("express");
 
 const TagsController = require("../controllers/tagsController");
+const ensureAuthenticated =  require("../middlewares/ensureAuthenticated");
 
 const tagsRouter = Router();
 
 const tagsController = new TagsController();
 
-tagsRouter.get("/:user_id", tagsController.index);
+tagsRouter.get("/", ensureAuthenticated, tagsController.index);
 
 
 module.exports = tagsRouter;
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,22 +1,14 @@
 const { Router } = require("express");
 
 const UsersController = require("../controllers/usersController");
+const ensureAuthenticated =  require("../middlewares/ensureAuthenticated")
 
 const usersRouter = Router();
-
-function myMiddleware(request, response, next) {
-  console.log("Você passou pelo Middleware");
-  if(!request.body.isAdmin){
-    return response.json({message: "user unauthorized"});
-  };
-
-  next();
-}
 
 const usersController = new UsersController();
 
 //usersRouter.use(myMiddleware); // para user em todos
 usersRouter.post("/", usersController.create);// se for usar o middleware em alguma rota especifica 
-usersRouter.put("/:id", usersController.update);
+usersRouter.put("/", ensureAuthenticated, usersController.update);
 
 module.exports = usersRouter;
